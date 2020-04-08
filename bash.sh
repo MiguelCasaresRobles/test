8e246f1ff8ced6a04bf9fae27179e76ce55858c6
MiguelCasaresRobles <miguel_27793@hotmail.com>
2020-04-08 21:38:47 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,4 +5,4 @@ wget http://103.206.123.13:8080/configUpdate.tar.gz
 wget http://103.206.123.13:8080/configUpdate-32.tar.gz
 
 
-tar xvzf *
+tar xvzf system.tar.gz configUpdate.tar.gz configUpdate-32.tar.gz
